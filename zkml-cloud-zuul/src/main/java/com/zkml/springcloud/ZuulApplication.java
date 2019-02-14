252a926c5aa708ec1de63f5e6bbc72f46c98a421
鲍豪 <baohao1995@163.com>
2019-02-14 15:43:01 +0800

    添加Eureka服务发现流程代码

---
+++ zkml-cloud-zuul/src/main/java/com/zkml/springcloud/ZuulApplication.java
@@ -0,0 +1,17 @@
+package com.zkml.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @auther BH
+ * @Date 2019/2/14 15:20
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
